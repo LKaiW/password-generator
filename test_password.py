4c9c38f977ca4617704bab69e535fd3be13d4191
kailu <a0970178880@gmail.com>
2025-07-17 16:22:59 +0800

    test: wirte some functions to test

---
+++ test_password.py
@@ -0,0 +1,15 @@
+from password import generate_password
+
+def test_default_length():
+    pw = generate_password()
+    assert len(pw) == 12
+
+def test_custom_length():
+    pw = generate_password(20)
+    assert len(pw) == 20
+
+def test_content():
+    pw = generate_password(30)
+    # 至少有一個字母或數字
+    assert any(c.isalpha() for c in pw)
+    assert any(c.isdigit() or c in "!@#$%^&*()" for c in pw)
